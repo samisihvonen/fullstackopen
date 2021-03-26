e697be6779941cb08b5f7036310d70179db26196
sami sihvonen <sami.sihvonen1984@gmail.com>
2021-03-26 23:29:05 +0200

    first commit

--- osa1/anekdootit/src/App.js
+++ osa1/anekdootit/src/App.js
@@ -1,34 +1,45 @@
 import React, { useState } from 'react'
 
-const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-  ]
-
+const App = (props) => {
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(points)
 
-  const random = () => {
-    return Math.floor(Math.random() * anecdotes.length)
-  }
-  console.log(random())
-
-  const handleClick = (random) => {
-    setSelected(randomItem)
+  const nextAnecdote = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length))
   }
 
-  const Button = () => <button onClick={handleClick}>Next Anectode</button>
-  var randomItem = anecdotes[Math.floor(Math.random() * anecdotes.length)]
+  const updateVotes = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+    console.log(votes)
+  }
+
+  const highestCount = Math.max(...votes)
+  const mostVoted = votes.indexOf(Math.max(...votes))
+
   return (
     <div>
-      <Button handleClick={() => setSelected(randomItem)} />
-      {anecdotes[selected]}
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} vote(s)</p>
+      <button onClick={updateVotes}>upvote</button>
+      <button onClick={nextAnecdote}>next anecdote</button>
+      <h1>Anecdote With the highest vote</h1>
+      <p>{anecdotes[mostVoted]}</p>
+      <p>has {highestCount} vote(s)</p>
     </div>
   )
 }
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+]
+
+const points = new Array(anecdotes.length).fill(0)
 
 export default App
